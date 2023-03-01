76308e06b4b4171671df2d5aad2fef90439014a5
t4top <temitope@yahoo.co.jp>
2023-03-01 14:42:07 +0100

    Add basic plugin files and define WP header fields

---
+++ solana-pay-for-woocommerce.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:       Solana Pay for WooCommerce
+ * Plugin URI:        https://github.com/t4top/solana-pay-for-woocommerce
+ * Description:       Add Solana Pay to your WooCommerce store. Solana Pay is a fast and open payments framework built on Solana blockchain.
+ * Version:           0.1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            t4top
+ * Author URI:        https://github.com/t4top/
+ * License:           GPLv3 or later
+ * License URI:       https://github.com/t4top/solana-pay-for-woocommerce/blob/main/LICENSE
+ * Text Domain:       solana-pay-for-wc
+ * Domain Path:       /languages
+ */
