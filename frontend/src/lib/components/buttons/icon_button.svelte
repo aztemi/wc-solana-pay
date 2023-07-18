a3c585984cb085e49a49fdfc148b6a5ba5aef68c
t4top <temitope@yahoo.co.jp>
2023-07-19 01:08:13 +0200

    Hide QR code by default on mobile devices

---
+++ frontend/src/lib/components/buttons/icon_button.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  export let disabled: boolean = false;
+</script>
+
+<button class={$$props.class} {disabled} on:click>
+  {#if $$slots["start-icon"]}
+    <i class="start-icon">
+      <slot name="start-icon" />
+    </i>
+  {/if}
+  <slot />
+  {#if $$slots["end-icon"]}
+    <i class="end-icon">
+      <slot name="end-icon" />
+    </i>
+  {/if}
+</button>
+
+<style lang="stylus">
+  button
+    display flex
+    align-items center
+    justify-content space-between
+    cursor pointer
+
+    i
+      display flex
+      align-items center
+      justify-content center
+      height 1.5rem
+      width 1.5rem
+
+    .start-icon
+      margin-right 0.7rem
+
+    .end-icon
+      margin-left 0.7rem
+
+</style>
--- frontend/src/lib/components/pay_with_qrcode.svelte
+++ frontend/src/lib/components/pay_with_qrcode.svelte
@@ -1,15 +1,40 @@
 <script>
+  import { onMount } from "svelte";
   import { order } from "../store/order.js";
+  import { isMobile } from "../utils/is_mobile.js";
+  import Icon from "./icons/icon.svelte";
   import QrCode from "./qrcode.svelte";
+  import IconButton from "./buttons/icon_button.svelte";
 
   export let link;
 
+  let hideQrcode = false;
   const { label, message } = $order;
+
+  onMount(() => {
+    hideQrcode = isMobile();
+  });
 </script>
 
-<span>Scan QR Code with Mobile Wallet</span>
-<div class="qrcode">
-  {#key link}
-    <QrCode {link} {label} {message} />
-  {/key}
-</div>
+{#if hideQrcode}
+  <IconButton on:click={() => (hideQrcode = false)} class="show_qr_button">
+    <Icon slot="start-icon" name="qr_scan" title="Show QR Code" />
+    Show QR Code
+  </IconButton>
+{:else}
+  <span>Scan QR Code with Mobile Wallet</span>
+  <div class="qrcode">
+    {#key link}
+      <QrCode {link} {label} {message} />
+    {/key}
+  </div>
+{/if}
+
+<style lang="stylus">
+  :global
+    .show_qr_button
+      border 1px solid var(--overlay_back_color)
+      border-radius 0.3rem
+      background-color transparent
+
+</style>
