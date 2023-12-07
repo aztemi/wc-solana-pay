3213a30e56f818a16939d94f3eb57102ddeafdc5
t4top <temitope@yahoo.co.jp>
2023-12-07 12:07:40 +0100

    Implement Svelte store for handling status notifications

---
+++ frontend/src/lib/components/notification/store.js
@@ -0,0 +1,84 @@
+import { writable } from "svelte/store";
+
+const TIMEOUT_DELAY = 10000;
+
+/**
+ * @typedef {(typeof STATE)[keyof typeof STATE]} StateType
+ * @typedef {(typeof EXIT)[keyof typeof EXIT]} ExitType
+ */
+
+export const STATE = {
+  NONE: 1,
+  LOADING: 2,
+  ERROR: 3,
+  OK: 4
+};
+
+export const EXIT = {
+  NONE: 1,
+  TIMEOUT: 2,
+  MANUAL: 3
+};
+
+export const notification = createNotificationStore();
+
+function createNotificationStore() {
+  const { subscribe, set, update } = writable({ count: 0, notices: [] });
+
+  /**
+   * @param {number} id
+   * @param {ExitType} exit
+   */
+  function timeout(id, exit) {
+    if (id > 0 && exit === EXIT.TIMEOUT) setTimeout(() => notification?.removeNotice(id), TIMEOUT_DELAY);
+  }
+
+  return {
+    subscribe,
+
+    reset: () => set({ count: 0, notices: [] }),
+
+    /**
+     * @param {string} message
+     */
+    addNotice: (message, status = STATE.NONE, exit = EXIT.NONE) => {
+      let id = -1;
+      update(last => {
+        let { count, notices } = last;
+        id = ++count;
+        notices.push({ id, message, status, exit });
+
+        return { ...last, count, notices };
+      });
+      timeout(id, exit);
+
+      return id;
+    },
+
+    /**
+     * @param {number} id
+     * @param {{message?:string, status?: StateType, exit?: ExitType, error?:string}} content
+     */
+    updateNotice: (id, content) =>
+      update(last => {
+        const { notices } = last;
+        const idx = notices.findIndex(n => n.id === id);
+        if (idx > -1) notices[idx] = { ...notices[idx], ...content };
+        timeout(id, content?.exit);
+
+        return { ...last, notices };
+      }),
+
+    /**
+     * @param {number} id
+     */
+    removeNotice: id =>
+      update(last => {
+        const { notices } = last;
+        const idx = notices.findIndex(n => n.id === id);
+        if (idx > -1) notices.splice(idx, 1);
+
+        return { ...last, notices };
+      })
+  };
+}
