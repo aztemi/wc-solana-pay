5d8e7185bf6e2336eb4cf2cc15f77453c947f721
t4top <temitope@yahoo.co.jp>
2024-03-23 13:44:13 +0100

    Fix 'process is not defined' error during page load

--- frontend/src/wc_solana_pay.js
+++ frontend/src/wc_solana_pay.js
@@ -1,4 +1,9 @@
+// @ts-nocheck
 import WC_Solana_Pay from "./wc_solana_pay.svelte";
+
+if (typeof window !== "undefined" && window.process === undefined) {
+  window.process = { browser: true, env: { ENVIRONMENT: "BROWSER" } };
+}
 
 const target = document.getElementById("wc_solana_pay_svelte_target");
 if (target) new WC_Solana_Pay({ target });
